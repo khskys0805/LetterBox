bf75db37a6b5b3fed4b32955ebbc8d0f7cd8ea48
hyeonah <hyeona0000@naver.com>
2022-12-26 19:24:16 +0900

    221226 프로젝트 생성

---
+++ src/main/java/com/proj/letterbox/LetterBoxApplication.java
@@ -0,0 +1,13 @@
+package com.proj.letterbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetterBoxApplication.class, args);
+    }
+
+}
