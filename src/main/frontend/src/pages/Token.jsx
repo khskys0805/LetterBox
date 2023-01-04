3deeeb45df1d467dff1d5756eee86ee8d57f5de8
ramda <91455804+gnfnfnr@users.noreply.github.com>
2023-01-04 16:48:05 +0900

    feat: check my login infomation

--- src/main/frontend/src/pages/Token.jsx
+++ src/main/frontend/src/pages/Token.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+
 export default function Token() {
   const params = window.location.search;
-  console.log(params.slice(6));
+  const [jwtToken, setJwtToken] = useState();
+  console.log(jwtToken);
   return (
-    <button
-      onClick={() => {
-        // axios
-        //   .get(`/login/oauth_kakao`, {
-        //     params: { code: params.slice(6) },
-        //   })
-        axios
-          .get(`/login/oauth_kakao${params}`)
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err));
-      }}
-    >
-      Token
-    </button>
+    <>
+      <button
+        onClick={() => {
+          axios
+            .get(`/login/oauth_kakao${params}`)
+            .then((res) => setJwtToken(res.headers.authorization))
+            .catch((err) => console.log(err));
+        }}
+      >
+        Token
+      </button>
+      <button
+        onClick={() => {
+          axios
+            .get("/me", {
+              headers: { authorization: jwtToken },
+            })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        }}
+      >
+        me
+      </button>
+    </>
   );
 }
