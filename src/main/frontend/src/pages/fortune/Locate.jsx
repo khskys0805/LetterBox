5b21f38787698933ff7df9b660fa22286e561ba1
ramda <91455804+gnfnfnr@users.noreply.github.com>
2023-01-07 01:45:45 +0900

    fix: select cell pointer position

--- src/main/frontend/src/pages/fortune/Locate.jsx
+++ src/main/frontend/src/pages/fortune/Locate.jsx
@@ -7,22 +7,35 @@ const LocateBox = styled.div`
   position: relative;
 `;
 const LocatePick = styled.div`
-  width: 100%;
-  height: 74%;
+  width: 90%;
+  height: 70%;
   position: absolute;
   top: 25%;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(4, 1fr);
+  clip-path: polygon(
+    35% -8%,
+    73% -1%,
+    102% 30%,
+    100% 70%,
+    71% 100%,
+    29% 100%,
+    0% 70%,
+    -2% 30%
+  );
+  left: 5%;
 `;
 
 const LocateCell = styled.div`
   width: 100%;
   height: 100%;
+  border-radius: 4px;
   &:hover {
     background: blue;
     cursor: pointer;
   }
+  background: ${(props) => props.current === props.number && "blue"};
 `;
 
 export default function Locate() {
@@ -30,26 +43,28 @@ export default function Locate() {
   const Next = () => <span>다음</span>;
   const navigate = useNavigate();
   const msg = Array.from({ length: 16 }, (_, idx) => idx);
-  const [locate, setLocate] = useState();
+  const [locate, setLocate] = useState(inputs.locate);
+  const ban = [0, 3, 12, 15];
   return (
     <>
       <LocateBox>
         <LocatePick>
           {msg.map((index) => {
-            return (
-              <LocateCell
-                onClick={(event) => {
-                  const arr = event.target.parentElement.children;
-                  for (let index = 0; index < arr.length; index++) {
-                    arr[index].style.background = "";
-                  }
-                  event.target.style.background = "yellow";
-                  // setLocate();
-                }}
-              >
-                {index}
-              </LocateCell>
-            );
+            if (ban.includes(index)) {
+              return <div />;
+            } else {
+              return (
+                <LocateCell
+                  key={index}
+                  number={index}
+                  current={locate}
+                  onClick={() => {
+                    console.log(ban.includes(index) ? "34" : "no");
+                    setLocate(index);
+                  }}
+                />
+              );
+            }
           })}
         </LocatePick>
         <img
@@ -61,7 +76,7 @@ export default function Locate() {
       <RoundButton
         Children={Next}
         onClick={() => {
-          // setInputs({ ...inputs, locate });
+          setInputs({ ...inputs, locate });
           navigate("/result");
         }}
       />
