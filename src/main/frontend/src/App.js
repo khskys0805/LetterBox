41e4fbfc66f5ff8b549266a8286763316debfeff
ramda <91455804+gnfnfnr@users.noreply.github.com>
2023-01-12 12:36:28 +0900

    feat: seperate private pages need login

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/join/Login";
+import Main from "./pages/Main";
+import { GlobalStyle } from "./style/Base";
+import Token from "./pages/join/Token";
+import PrivateRoute from "./PrivateRoute";
+import Messages from "./pages/Messages";
 import Chatting from "./pages/Chatting";
 import Complete from "./pages/fortune/Complete";
 import Nickname from "./pages/fortune/Nickname";
 import Question from "./pages/Question";
-import Login from "./pages/join/Login";
-import Main from "./pages/Main";
-import { GlobalStyle } from "./style/Base";
-import Messages from "./pages/Messages";
-import Storage from "./pages/Storage";
-import Token from "./pages/join/Token";
 import Hints from "./pages/fortune/Hints";
 import Connct from "./pages/fortune/Connct";
 import Content from "./pages/fortune/Content";
 import Locate from "./pages/fortune/Locate";
 import CreateBox from "./pages/box/CreateBox";
 import UserBox from "./pages/box/UserBox";
-import PrivateRoute from "./components/PrivateRoute";
+import LetterBox from "./pages/LetterBox";
 
 function App() {
   return (
@@ -26,18 +26,21 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/auth" element={<Login />} />
           <Route path="/kakao/login" element={<Token />} />
-          {/* <Route path="/" element={<Chatting />} /> */}
-          <Route element={<Question />}>
-            <Route path="/question/nickname" element={<Nickname />} />
-            <Route path="/question/hints" element={<Hints />} />
-            <Route path="/question/connect" element={<Connct />} />
-            <Route path="/question/content" element={<Content />} />
-            <Route path="/question/locate" element={<Locate />} />
+          <Route element={<PrivateRoute />}>
+            <Route element={<Question />}>
+              <Route path="/question/nickname" element={<Nickname />} />
+              <Route path="/question/hints" element={<Hints />} />
+              <Route path="/question/connect" element={<Connct />} />
+              <Route path="/question/content" element={<Content />} />
+              <Route path="/question/locate" element={<Locate />} />
+            </Route>
+            <Route path="/chatting" element={<Chatting />} />
+            <Route path="/result" element={<Complete />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/createBox" element={<CreateBox />} />
+            <Route path="/userbox" element={<UserBox />} />
           </Route>
-          <Route path="/result" element={<Complete />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/createBox" element={<CreateBox />} />
-          <Route path="/userbox" element={<UserBox />} />
+          <Route path="/box" element={<LetterBox />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/main/frontend/src/PrivateRoute.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function PrivateRoute() {
+  return localStorage.getItem("jwt") ? <Outlet /> : <Navigate to="/auth" />;
+}
