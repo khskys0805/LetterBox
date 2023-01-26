9308f1c770633047ac24059b1d7f829c563feed0
ramda <91455804+gnfnfnr@users.noreply.github.com>
2023-01-26 17:10:13 +0900

    Revert "fix: change clock letter"
    This reverts commit e1d2047aa225223f3b29fd09aa748e0cb38ff07f.

--- src/main/java/com/proj/letterbox/config/LetterScheduled.java
+++ src/main/java/com/proj/letterbox/config/LetterScheduled.java
@@ -13,8 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
@@ -29,27 +27,22 @@ public class LetterScheduled {
     private LetterService letterService;
     @Autowired
     private LetterRepository letterRepository;
-    @Autowired
-    private LetterBoxRepository letterBoxRepository;
 
     //나중에 cron="0 0 0 * * ?"
     @Scheduled(cron="0 * * * * ?")
     public void LetterCron() {
-        Calendar cal1 = Calendar.getInstance();
-        Calendar cal2 = Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();
         //나중에 변경
-        cal1.set(2023, 0, 27);
-        long dDay = cal1.getTimeInMillis();
+        cal.set(2023, 2, 1);
+        long dDay = cal.getTimeInMillis();
         System.out.println(dDay);
-        //long now = System.currentTimeMillis();
-        long now = cal2.getTimeInMillis();
-        System.out.println(LocalDate.now());
+        long now = System.currentTimeMillis();
+        System.out.println(now);
         long result = dDay - now;
         System.out.println(result);
 
         //int day = (int) (result / 1000 / 60 / 60 / 24);
         int min = (int) (result / 1000 / 60);
-        System.out.println(min);
 
         List<LetterBox> letterboxes = letterBoxService.findAll();
         //day >= 0 && day <= 6
@@ -63,15 +56,14 @@ public class LetterScheduled {
                     }
                 }
                 //int open = unopen / (day + 1);
-                int open = 1;
+                int open = 2;
                 Collections.shuffle(letters);
                 for (int i = 0; i < open; i++) {
                     Letter letter = letters.get(i);
                     letter.setOpen(true);
                     letterRepository.save(letter);
-                    letterBox.getLetterList().remove(new LetterList(letter.getLetterlocation(), letter.getLetterId(), false));
-                    letterBox.getLetterList().add(new LetterList(letter.getLetterlocation(), letter.getLetterId(), true));
-                    letterBoxRepository.save(letterBox);
+                    letterBox.getLetterList().remove(new LetterList(letter.getLetterId(), letter.getLetterlocation(), false));
+                    letterBox.getLetterList().add(new LetterList(letter.getLetterId(), letter.getLetterlocation(), true));
                     System.out.println(letter.isOpen());
                 }
             }
