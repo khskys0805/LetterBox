d30f4c69b69f2b1af02231233976b12cecb5f83c
ramda <91455804+gnfnfnr@users.noreply.github.com>
2023-02-15 14:26:42 +0900

    feat: put logo in header

--- src/main/frontend/src/pages/nav/Header.jsx
+++ src/main/frontend/src/pages/nav/Header.jsx
@@ -3,11 +3,21 @@ import { THEME } from "../../constant";
 import { useNavigate } from "react-router-dom";
 
 const HeaderCover = styled.header`
-  padding: 15px;
+  padding: 0 15px;
   background-color: ${THEME};
+`;
+
+const HeaderInside = styled.div`
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
+  max-width: 700px;
+  margin: 0 auto;
+`;
+
+const LogoImg = styled.img`
+  width: 55px;
+  object-fit: contain;
 `;
 
 export function Header() {
@@ -15,25 +25,34 @@ export function Header() {
 
   return (
     <HeaderCover>
-      {localStorage.getItem("jwt") ? (
-        <div
+      <HeaderInside>
+        <LogoImg
+          src={require("../../img/logo.png")}
+          alt="로고"
           onClick={() => {
-            localStorage.removeItem("jwt");
-            window.location.reload();
+            navigate("/");
           }}
-        >
-          로그아웃
-        </div>
-      ) : (
-        <div
-          onClick={() => {
-            localStorage.removeItem("jwt");
-            navigate("/auth");
-          }}
-        >
-          로그인
-        </div>
-      )}
+        />
+        {localStorage.getItem("jwt") ? (
+          <div
+            onClick={() => {
+              localStorage.removeItem("jwt");
+              window.location.reload();
+            }}
+          >
+            로그아웃
+          </div>
+        ) : (
+          <div
+            onClick={() => {
+              localStorage.removeItem("jwt");
+              navigate("/auth");
+            }}
+          >
+            로그인
+          </div>
+        )}
+      </HeaderInside>
     </HeaderCover>
   );
 }
